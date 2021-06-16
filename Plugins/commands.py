b35395354c092bbfd3beed1abdd8a2e46bb3f588
scprojectslk <80474245+scprojectslk@users.noreply.github.com>
2021-06-16 21:51:43 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -22,7 +22,7 @@ start_button=InlineKeyboardMarkup(
                   InlineKeyboardButton("📄 BOT STATUS", callback_data = "status_data")
               ], 
               [
-                  InlineKeyboardButton("📫 UPDATES", url = "https://t.me/Ts_bots"), 
+                  InlineKeyboardButton("📫 UPDATES", url = "https://t.me/SCProjectsLK"), 
                   InlineKeyboardButton("📕 ABOUT", callback_data = "about_data")
               ], 
               [
